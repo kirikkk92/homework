575ecbe63ece92d9c191f8f8014da4af67c5af06
kirikkk92 <ykrop1992@mail.ru>
2019-12-12 20:39:23 +0300

    add ispr

--- homework3/homework3'b/src/com/company/Main.java
+++ homework3/homework3'b/src/com/company/Main.java
@@ -1,24 +1,18 @@
 package com.company;
 
 public class Main {
-
+// исправлен вложенный цикл
     public static void main(String[] args) {
-    double [] arr = {47,55,9.-4,3,0};
+    double [] arr = {-1,55,9,4,3,0};
     arr  = min (arr);
-    System.out.println(arr[0]);
     }
-    public static double[] min (double []a) {
-
-        for (int i = 0; i < a.length; i++) {
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[i] > a[j]) {
-                    double buf = a[i];
-                    a[i] = a[j];
-                    a[j] = buf;
-                }
-            }
-
-        }return a;
+    public static double [] min (double []a) {
+        double min = a[0];
+        for (int i = 0; i < a.length -1; i++) {
+            if (min > a[i+1])
+            min = a[i+1];
+        }System.out.println(min);
+        return a;
     }
 }
 
--- homework3/homework3'i/src/com/company/Main.java
+++ homework3/homework3'i/src/com/company/Main.java
@@ -1,21 +1,20 @@
 package com.company;
 
 public class Main {
-
+//исправлен вложенный цикл
     public static void main(String[] args) {
-        int [] arr = {1,2,3,4,5,6,7,77};
+        int [] arr = {-3,-2,3,4,5,6,7,89,-100,};
         arr  = isSorted(arr);
     }
     public static int []  isSorted  (int [] arr ) {
         boolean sorted = true;
         int i;
-        for (i = 0; i < arr.length; i++) {
-            for (int j = i + 1;j < arr.length;j++) {
-                if (arr[i] > arr[j]) {
-                    sorted = false;
-                    break;
-                }
+        for (i = 0; i < arr.length-1; i++) {
+            if (arr[i] >= arr[i+1]) {
+                sorted = false;
             }
+
+
         }System.out.print("The array is sorted: ");
         System.out.println(sorted);
         return arr;
